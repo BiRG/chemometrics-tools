e37f3d195db443a7b897c79e2adf68829a9fced1
Daniel Foose <dpfoose@gmail.com>
2020-01-09 16:04:18 -0500

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='birg_chemometrics_tools',
-    version='20.01',
+    version='20.1',
     author='BiRG @ Wright State University',
     author_email='foose.3@wright.edu',
     description='Chemometric Tools for NMR Metabolomics',
